d77758da8ed34cde469f3e6ae7fc6ab2018afc5a
tesfayealex <tesfayealemayehu27@gmail.com>
2022-07-16 21:39:16 +0300

    added contracts on admin panel

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { EthProvider } from "./contexts/EthContext";
+import Intro from "./components/Intro/";
+import Setup from "./components/Setup";
+import Demo from "./components/Demo";
+import Footer from "./components/Footer";
+import "./App.css";
+import Main from "./components/CollectData";
+
+function App() {
+  return (
+    <EthProvider>
+      <div id="App" >
+        <div className="container">
+           <Intro />
+          <hr />
+          <Setup />
+          <hr />
+          <Demo />
+          <hr />
+          <Main/>
+          <hr />
+          {/* <Footer /> */}
+        </div>
+      </div>
+    </EthProvider>
+  );
+}
+
+export default App;
